cbfc755c3a380ab7f5b708b308d18c2a1bc46b9d
ahmed93 <hero.20112012@gmail.com>
2014-05-21 00:01:57 +0200

    decode ++

--- src/stages/Decode.java
+++ src/stages/Decode.java
@@ -2,23 +2,46 @@ package stages;
 
 import exception.NoSuchInstructionException;
 import exception.NoSuchRegisterException;
+import exception.OverFlowException;
 import abstracts.Instruction;
+import abstracts.RFormat;
 import instructionSet.InstructionString;
 import instructions.*;
 
 public class Decode {
 
-	public static Instruction id(InstructionString instruc) throws NoSuchInstructionException, NoSuchRegisterException {
+	public static Instruction id(InstructionString instruc) throws NoSuchInstructionException, NoSuchRegisterException, OverFlowException {
+		if(instruc == null)
+			throw new NoSuchInstructionException("No such instruchtion found");
+		
+		// getting the instruction class name
 		String opc = instruc.getOpcode();
-		String[] rest = instruc.getRest().split(",");
+		String className = instruc.getOpcode().toLowerCase();
+		className = Character.toUpperCase(opc.charAt(0)) + opc.substring(1);
+		Class instructionName;
+		Object instructionClass;
+		try {
+			instructionName = Class.forName(className);
+			instructionClass = instructionName.newInstance();
+		} catch (ClassNotFoundException | InstantiationException | IllegalAccessException e) {
+			throw new NoSuchInstructionException("No such instruchtion found");
+		}
+		
+		String[] address_String = instruc.getRest().split(",");
 		String rs,rt,rd;
 		short address;
-
+		
+		if(instructionClass instanceof RFormat)
+		
+		
 		switch (opc.toLowerCase()) {
 		case "add":
-			rs = rest[1];
-			rt = rest[2];
-			rd = rest[0];
+			rs = address_String[1];
+			rt = address_String[2];
+			rd = address_String[0];
+			int value = Integer.parseInt(address_String[2]);
+			if (value > Short.MAX_VALUE || value < Short.MIN_VALUE) 
+				throw new OverFlowException("");
 			return new Add(rs, rt, rd);
 			
 		case "addi":
